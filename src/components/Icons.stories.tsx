3084484ef70129fdd72b7b5fe3df87775c8f5a22
Ako <a.veysinejad@gmail.com>
2025-04-30 13:07:03 +0330

    Add new icons Requisition, Stock, Transactions, TransferList, Warehouse, and WordFile

--- src/components/Icons.stories.tsx
+++ src/components/Icons.stories.tsx
@@ -2,70 +2,72 @@ import * as React from "react";
 import type { Meta } from "@storybook/react";
 
 import { AdobeAcrobat } from "./AdobeAcrobat";
-import { Calendar } from "./Calendar";
-import { Star } from "./Star";
-import { StarFilled } from "./StarFilled";
-import { SquareUpLeft } from "./SquareUpLeft";
-import { Minus } from "./Minus";
-import { SquareArrowDownRight } from "./SquareArrowDownRight";
-import { SquareCaretUp } from "./SquareCaretUp";
-import { SquareArrowUpLeft } from "./SquareArrowUpLeft";
-import { SquareCaretDown } from "./SquareCaretDown";
-import { SquareDownRight } from "./SquareDownRight";
-import { Slash } from "./Slash";
-import { SliderUp } from "./SliderUp";
-import { Snowflake } from "./Snowflake";
-import { Send } from "./Send";
-import { SidebarLeft } from "./SidebarLeft";
-import { Sigma } from "./Sigma";
-import { Selected } from "./Selected";
-import { Refresh } from "./Refresh";
-import { ScaleBalanced } from "./ScaleBalanced";
-import { Question } from "./Question";
-import { Reply } from "./Reply";
-import { RePost } from "./RePost";
-import { RolePermission } from "./RolePermission";
-import { Projects } from "./Projects";
-import { QCApprove } from "./QCApprove";
-import { ListSearch } from "./ListSearch";
-import { CircleChecked } from "./CircleChecked";
-import { PaintBrush } from "./PaintBrush";
-import { Pencil } from "./Pencil";
-import { Plus } from "./Plus";
-import { Print } from "./Print";
-import { ProductionSchedule } from "./ProductionSchedule";
-import { Progress } from "./Progress";
-import { Members } from "./Members";
-import { SquareMinus } from "./SquareMinus";
-import { Move } from "./Move";
-import { NoteBadge } from "./NoteBadge";
-import { Note } from "./Note";
-import { ObjectGroup } from "./ObjectGroup";
-import { Magnifier } from "./Magnifier";
-import { MagnifierPlus } from "./MagnifierPlus";
-import { MagnifierMinus } from "./MagnifierMinus";
-import { MapPin } from "./MapPin";
-import { Measuring } from "./Measuring";
-import { LinkSlash } from "./LinkSlash";
-import { Link } from "./Link";
-import { List } from "./List";
-import { LiftTruck } from "./LiftTruck";
-import { LockOpen } from "./LockOpen";
-import { Lock } from "./Lock";
+import { AlignCenter } from "./AlignCenter";
+import { AlignJustify } from "./AlignJustify";
+import { AlignLeft } from "./AlignLeft";
+import { AlignRight } from "./AlignRight";
+import { Archive } from "./Archive";
+import { ArrowLeft } from "./ArrowLeft";
 import { ArrowLeftToLine } from "./ArrowLeftToLine";
-import { Import } from "./Import";
-import { InstallGlass } from "./InstallGlass";
-import { InstallRail } from "./InstallRail";
-import { LayerGroup } from "./LayerGroup";
-import { Library } from "./Library";
-import { Image } from "./Image";
-import { Gear } from "./Gear";
-import { Google } from "./Google";
-import { GridDividers } from "./GridDividers";
-import { Grip } from "./Grip";
-import { Grid } from "./Grid";
-import { House } from "./House";
-import { Save } from "./Save";
+import { ArrowRight } from "./ArrowRight";
+import { ArrowRightFromLine } from "./ArrowRightFromLine";
+import { ArrowRotateRight } from "./ArrowRotateRight";
+import { ArrowsInward } from "./ArrowsInward";
+import { ArrowsMaximize } from "./ArrowsMaximize";
+import { ArrowsMinimize } from "./ArrowsMinimize";
+import { ArrowsOutward } from "./ArrowsOutward";
+import { Assemblies } from "./Assemblies";
+import { Attachment } from "./Attachment";
+import { Ban } from "./Ban";
+import { Bars } from "./Bars";
+import { BarsSort } from "./BarsSort";
+import { Bell } from "./Bell";
+import { Board } from "./Board";
+import { Breakdown } from "./Breakdown";
+import { Calendar } from "./Calendar";
+import { Camera } from "./Camera";
+import { Check } from "./Check";
+import { ChevronDown } from "./ChevronDown";
+import { ChevronLeft } from "./ChevronLeft";
+import { ChevronLeftEnd } from "./ChevronLeftEnd";
+import { ChevronRight } from "./ChevronRight";
+import { ChevronRightEnd } from "./ChevronRightEnd";
+import { ChevronUp } from "./ChevronUp";
+import { ChevronsDown } from "./ChevronsDown";
+import { ChevronsLeft } from "./ChevronsLeft";
+import { ChevronsRight } from "./ChevronsRight";
+import { ChevronsUp } from "./ChevronsUp";
+import { Circle } from "./Circle";
+import { CircleChecked } from "./CircleChecked";
+import { CircleDollar } from "./CircleDollar";
+import { CircleInfo } from "./CircleInfo";
+import { CirclePlus } from "./CirclePlus";
+import { CircleXmark } from "./CircleXmark";
+import { Clock } from "./Clock";
+import { Clone } from "./Clone";
+import { Comma } from "./Comma";
+import { Comments } from "./Comments";
+import { CommentsBadge } from "./CommentsBadge";
+import { Company } from "./Company";
+import { Connect } from "./Connect";
+import { Contract } from "./Contract";
+import { Copy } from "./Copy";
+import { Dashboard } from "./Dashboard";
+import { Deficiency } from "./Deficiency";
+import { DeficiencyList } from "./DeficiencyList";
+import { Delivery } from "./Delivery";
+import { Download } from "./Download";
+import { Driver } from "./Driver";
+import { Elipsis } from "./Elipsis";
+import { ElipsisVertical } from "./ElipsisVertical";
+import { ElipsisVerticalStroke } from "./ElipsisVerticalStroke";
+import { Email } from "./Email";
+import { Estimate } from "./Estimate";
+import { Exclusion } from "./Exclusion";
+import { Expand } from "./Expand";
+import { Export } from "./Export";
+import { Eye } from "./Eye";
+import { EyeSlash } from "./EyeSlash";
 import { FileChart } from "./FileChart";
 import { FileChartColumn } from "./FileChartColumn";
 import { FileExcel } from "./FileExcel";
@@ -76,72 +78,87 @@ import { FilterSlash } from "./FilterSlash";
 import { FlagPennant } from "./FlagPennant";
 import { FolderClosed } from "./FolderClosed";
 import { FolderOpen } from "./FolderOpen";
-import { Elipsis } from "./Elipsis";
-import { ElipsisVertical } from "./ElipsisVertical";
-import { ElipsisVerticalStroke } from "./ElipsisVerticalStroke";
-import { Email } from "./Email";
-import { Exclusion } from "./Exclusion";
-import { Expand } from "./Expand";
-import { Export } from "./Export";
-import { Eye } from "./Eye";
-import { EyeSlash } from "./EyeSlash";
-import { Comments } from "./Comments";
-import { Company } from "./Company";
-import { Connect } from "./Connect";
-import { Copy } from "./Copy";
-import { Dashboard } from "./Dashboard";
-import { Deficiency } from "./Deficiency";
-import { DeficiencyList } from "./DeficiencyList";
-import { Download } from "./Download";
-import { Driver } from "./Driver";
-import { CircleDollar } from "./CircleDollar";
-import { CircleInfo } from "./CircleInfo";
-import { CirclePlus } from "./CirclePlus";
-import { CircleXmark } from "./CircleXmark";
-import { Clock } from "./Clock";
-import { Comma } from "./Comma";
-import { Clone } from "./Clone";
-import { CommentsBadge } from "./CommentsBadge";
-import { ChevronDown } from "./ChevronDown";
-import { ChevronRight } from "./ChevronRight";
-import { ChevronUp } from "./ChevronUp";
-import { ChevronLeft } from "./ChevronLeft";
-import { ChevronsDown } from "./ChevronsDown";
-import { ChevronsLeft } from "./ChevronsLeft";
-import { ChevronsRight } from "./ChevronsRight";
-import { ChevronsUp } from "./ChevronsUp";
-import { Bars } from "./Bars";
-import { Ban } from "./Ban";
-import { Bell } from "./Bell";
-import { BarsSort } from "./BarsSort";
-import { Board } from "./Board";
-import { Breakdown } from "./Breakdown";
-import { ArrowLeft } from "./ArrowLeft";
-import { ArrowRight } from "./ArrowRight";
-import { ArrowRightFromLine } from "./ArrowRightFromLine";
-import { ArrowRotateRight } from "./ArrowRotateRight";
-import { ArrowsInward } from "./ArrowsInward";
-import { ArrowsOutward } from "./ArrowsOutward";
-import { ArrowsMaximize } from "./ArrowsMaximize";
-import { ArrowsMinimize } from "./ArrowsMinimize";
-import { Assemblies } from "./Assemblies";
-import { Attachment } from "./Attachment";
-import { AlignCenter } from "./AlignCenter";
-import { AlignJustify } from "./AlignJustify";
-import { AlignLeft } from "./AlignLeft";
-import { AlignRight } from "./AlignRight";
-import { Archive } from "./Archive";
+import { Forward } from "./Forward";
+import { Gear } from "./Gear";
+import { Google } from "./Google";
+import { Grid } from "./Grid";
+import { GridDividers } from "./GridDividers";
+import { Grip } from "./Grip";
+import { House } from "./House";
+import { Image } from "./Image";
+import { ImageFile } from "./ImageFile";
+import { Import } from "./Import";
+import { InstallGlass } from "./InstallGlass";
+import { InstallRail } from "./InstallRail";
+import { Items } from "./Items";
+import { LayerGroup } from "./LayerGroup";
+import { Library } from "./Library";
+import { LiftTruck } from "./LiftTruck";
+import { Link } from "./Link";
+import { LinkSlash } from "./LinkSlash";
+import { List } from "./List";
+import { ListSearch } from "./ListSearch";
+import { Lock } from "./Lock";
+import { LockOpen } from "./LockOpen";
+import { Magnifier } from "./Magnifier";
+import { MagnifierMinus } from "./MagnifierMinus";
+import { MagnifierPlus } from "./MagnifierPlus";
+import { MapPin } from "./MapPin";
+import { Measuring } from "./Measuring";
+import { Members } from "./Members";
+import { Minus } from "./Minus";
+import { Move } from "./Move";
+import { Note } from "./Note";
+import { NoteBadge } from "./NoteBadge";
+import { ObjectGroup } from "./ObjectGroup";
+import { PaintBrush } from "./PaintBrush";
+import { Pencil } from "./Pencil";
+import { Plus } from "./Plus";
+import { Print } from "./Print";
+import { ProductionSchedule } from "./ProductionSchedule";
+import { Progress } from "./Progress";
+import { Projects } from "./Projects";
+import { QCApprove } from "./QCApprove";
+import { Question } from "./Question";
+import { Refresh } from "./Refresh";
+import { Reply } from "./Reply";
+import { Reports } from "./Reports";
+import { RePost } from "./RePost";
+import { Requisition } from "./Requisition";
+import { RolePermission } from "./RolePermission";
+import { Save } from "./Save";
+import { ScaleBalanced } from "./ScaleBalanced";
+import { Selected } from "./Selected";
+import { Send } from "./Send";
+import { SidebarLeft } from "./SidebarLeft";
+import { Sigma } from "./Sigma";
+import { Slash } from "./Slash";
+import { SliderUp } from "./SliderUp";
+import { Snowflake } from "./Snowflake";
+import { Spinner } from "./Spinner";
+import { SquareArrowDownRight } from "./SquareArrowDownRight";
+import { SquareArrowUpLeft } from "./SquareArrowUpLeft";
+import { SquareCaretDown } from "./SquareCaretDown";
+import { SquareCaretUp } from "./SquareCaretUp";
+import { SquareDownRight } from "./SquareDownRight";
+import { SquareMinus } from "./SquareMinus";
+import { SquareUpLeft } from "./SquareUpLeft";
+import { Star } from "./Star";
+import { StarFilled } from "./StarFilled";
+import { Stock } from "./Stock";
+import { Transactions } from "./Transactions";
 import { Transfer } from "./Transfer";
+import { TransferList } from "./TransferList";
 import { Trashcan } from "./Trashcan";
 import { Treeview } from "./Treeview";
 import { Triangle } from "./Triangle";
 import { Upload } from "./Upload";
 import { User } from "./User";
 import { ValueEngineering } from "./ValueEngineering";
+import { Warehouse } from "./Warehouse";
 import { Warning } from "./Warning";
+import { WordFile } from "./WordFile";
 import { XMark } from "./XMark";
-import { Check } from "./Check";
-import { Circle } from "./Circle";
 
 const meta: Meta = {
   title: "Icons",
@@ -339,6 +356,12 @@ export const Icons = () => {
           <Calendar />
         </div>
         <div>
+          <div className="icon-name" data-keywords="camera, video, file, meeting">
+            Camera
+          </div>
+          <Camera />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="check, mark">
             Check
           </div>
@@ -357,10 +380,22 @@ export const Icons = () => {
           <ChevronLeft />
         </div>
         <div>
+          <div className="icon-name" data-keywords="chevron, arrow, left, end">
+            ChevronLeftEnd
+          </div>
+          <ChevronLeftEnd />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="chevron, arrow, right">
             ChevronRight
           </div>
           <ChevronRight />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="chevron, arrow, right, end">
+            ChevronRightEnd
+          </div>
+          <ChevronRightEnd />
         </div>
         <div>
           <div className="icon-name" data-keywords="chevron, arrow, up">
@@ -471,6 +506,12 @@ export const Icons = () => {
           <Connect />
         </div>
         <div>
+          <div className="icon-name" data-keywords="contract, paper, pen">
+            Contract
+          </div>
+          <Contract />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="copy, duplicate">
             Copy
           </div>
@@ -493,6 +534,12 @@ export const Icons = () => {
             DeficiencyList
           </div>
           <DeficiencyList />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="delivery, truck">
+            Delivery
+          </div>
+          <Delivery />
         </div>
         <div>
           <div className="icon-name" data-keywords="download, arrow">
@@ -529,6 +576,12 @@ export const Icons = () => {
             Email
           </div>
           <Email />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="estimate, calculator">
+            Estimate
+          </div>
+          <Estimate />
         </div>
         <div>
           <div className="icon-name" data-keywords="exclusion, square, exclamation">
@@ -621,6 +674,12 @@ export const Icons = () => {
           <FolderOpen />
         </div>
         <div>
+          <div className="icon-name" data-keywords="forward, reply, arrow, right">
+            Forward
+          </div>
+          <Forward />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="gear, settings">
             Gear
           </div>
@@ -663,6 +722,12 @@ export const Icons = () => {
           <Image />
         </div>
         <div>
+          <div className="icon-name" data-keywords="image, picture, file">
+            ImageFile
+          </div>
+          <ImageFile />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="import, arrow">
             Import
           </div>
@@ -679,6 +744,12 @@ export const Icons = () => {
             InstallRail
           </div>
           <InstallRail />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="items, box">
+            Items
+          </div>
+          <Items />
         </div>
         <div>
           <div className="icon-name" data-keywords="layer, group">
@@ -867,10 +938,22 @@ export const Icons = () => {
           <Reply />
         </div>
         <div>
+          <div className="icon-name" data-keywords="report, list">
+            Reports
+          </div>
+          <Reports />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="repost, share, arrows">
             RePost
           </div>
           <RePost />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="requisition, arrow up, list">
+            Requisition
+          </div>
+          <Requisition />
         </div>
         <div>
           <div className="icon-name" data-keywords="role, permission, shield, person, user">
@@ -933,6 +1016,12 @@ export const Icons = () => {
           <Snowflake />
         </div>
         <div>
+          <div className="icon-name" data-keywords="spinner, refresh">
+            Spinner
+          </div>
+          <Spinner />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="square, arrow, up, left">
             SquareArrowUpLeft
           </div>
@@ -987,10 +1076,28 @@ export const Icons = () => {
           <StarFilled />
         </div>
         <div>
+          <div className="icon-name" data-keywords="stock, home, house, shop">
+            Stock
+          </div>
+          <Stock />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="arrow up, arrow down, transfer, exchange">
+            Transactions
+          </div>
+          <Transactions />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="transfer, arrow">
             Transfer
           </div>
           <Transfer />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="transfer, arrows, list">
+            TransferList
+          </div>
+          <TransferList />
         </div>
         <div>
           <div className="icon-name" data-keywords="trashcan, delete, remove">
@@ -1029,10 +1136,22 @@ export const Icons = () => {
           <ValueEngineering />
         </div>
         <div>
+          <div className="icon-name" data-keywords="warehouse, storage, shop, stock, house, building">
+            Warehouse
+          </div>
+          <Warehouse />
+        </div>
+        <div>
           <div className="icon-name" data-keywords="warning, exclamation, triangle">
             Warning
           </div>
           <Warning />
+        </div>
+        <div>
+          <div className="icon-name" data-keywords="word, file">
+            WordFile
+          </div>
+          <WordFile />
         </div>
         <div>
           <div className="icon-name" data-keywords="x, mark, close">
