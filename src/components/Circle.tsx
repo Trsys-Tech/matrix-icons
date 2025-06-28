3322ea480b1386720a563fea858346a4ee87e14d
Ako <a.veysinejad@gmail.com>
2025-06-28 12:43:56 +0330

    fix: update Circle icon

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -2,8 +2,8 @@ import { SVGProps } from "react";
 
 export const Circle: React.FC<SVGProps<SVGSVGElement>> = props => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" {...props}>
-      <circle cx="12" cy="12" r="12"></circle>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24" {...props}>
+      <circle cx="12" cy="12" r="8"></circle>
     </svg>
   );
 };
